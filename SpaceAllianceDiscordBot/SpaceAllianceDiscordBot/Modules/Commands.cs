824785fd05d1a027b2c0671b3455b0226b9a5b26
Malantrius <tttdddwork@gmail.com>
2020-12-12 12:47:18 +0200

    Importing project

---
+++ SpaceAllianceDiscordBot/SpaceAllianceDiscordBot/Modules/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace SpaceAllianceDiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+            
+        }
+
+    }
+}
